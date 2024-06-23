7bf1fd8b73bd292b0c249f75a9dbb076660515d8
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2024-06-23 11:03:41 -0400

    ReSetup

---
+++ src/main/java/com/silicateseer/beholdersvariety/BeholdersVarietyDataGenerator.java
@@ -0,0 +1,11 @@
+package com.silicateseer.beholdersvariety;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeholdersVarietyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

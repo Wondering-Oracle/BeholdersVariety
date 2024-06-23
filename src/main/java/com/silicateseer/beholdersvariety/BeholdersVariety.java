358435e316dab5273f6c3b1027dfadb4093e9098
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2024-06-23 12:47:28 -0400

    More Setup

--- src/main/java/com/silicateseer/beholdersvariety/BeholdersVariety.java
+++ src/main/java/com/silicateseer/beholdersvariety/BeholdersVariety.java
@@ -6,10 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BeholdersVariety implements ModInitializer {
+
+	public static final String MODID = "beholdersvariety";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("beholdersvariety");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/com/silicateseer/beholdersvariety/BeholdersVarietyClient.java
@@ -0,0 +1,10 @@
+package com.silicateseer.beholdersvariety;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeholdersVarietyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
